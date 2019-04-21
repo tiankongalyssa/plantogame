f5a6e8f3f4b70d0920da5600c05b4677c44a28ef
itcast <tiankongalyssa@gmail.com>
2019-04-21 10:58:49 +0800

    查询预测详情修改为100条。 10:58

--- plantogame_plantogame/src/main/java/com/sky/plantogame/dao/PlanCreateDao.java
+++ plantogame_plantogame/src/main/java/com/sky/plantogame/dao/PlanCreateDao.java
@@ -19,7 +19,7 @@ public interface PlanCreateDao extends JpaRepository<PlanCreate,String>,JpaSpeci
     @Query(value = "SELECT * from tb_plan_create WHERE type=?1 AND gamekey=?2 AND present=?3",nativeQuery = true)
     PlanCreate findNewPlant(String type, String gameKey, String present);
 
-    @Query(value = "SELECT * FROM tb_plan_create WHERE  type =?1  AND gamekey = ?2 AND create_time BETWEEN DATE(NOW())  AND gamekey = '' ORDER BY create_time DESC limit 0,300 ",nativeQuery = true)
+    @Query(value = "SELECT * FROM tb_plan_create WHERE  type =?1  AND gamekey = ?2 AND create_time BETWEEN DATE(NOW())  AND gamekey = '' ORDER BY create_time DESC limit 0,50 ",nativeQuery = true)
     List<PlanCreate> findTodayList(String type,String gameKey);
     @Query(value = "SELECT * FROM tb_plan_create WHERE gamekey=?1 AND type=?2 ORDER BY create_time DESC LIMIT 1,1",nativeQuery = true)
     PlanCreate findBefore(String gameKey,String type);
