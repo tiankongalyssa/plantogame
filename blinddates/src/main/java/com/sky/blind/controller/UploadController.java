012b4dcb93d3f1101bd78765727b7298a8908f6a
anyetanxi2017 <tiankongalyssa@outlook.com>
2019-05-22 15:20:36 +0800

    后台管理

---
+++ blinddates/src/main/java/com/sky/blind/controller/UploadController.java
@@ -0,0 +1,35 @@
+package com.sky.blind.controller;
+
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/upload")
+@CrossOrigin
+public class UploadController {
+    @PostMapping
+    public Result singleFileupload(MultipartFile file) {
+        if (file.isEmpty()) {
+            return new Result(false, StatusCode.ERROR, "文件为空");
+        }
+        try {
+            String fileName = file.getOriginalFilename();
+            File img = new File("/home/upload/" + System.currentTimeMillis() + fileName);
+//            File img = new File("D:/upload/" + System.currentTimeMillis() + fileName);
+            fileName = "D:/upload/"+fileName;
+            file.transferTo(img);
+            return new Result(true, StatusCode.OK, "上传成功",fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
