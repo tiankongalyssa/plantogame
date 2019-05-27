dfdf325f5217b10c4ddf9af2d9a95caa090be516
anyetanxi2017 <tiankongalyssa@outlook.com>
2019-05-27 19:20:08 +0800

    19:16

--- blinddates/src/main/java/com/sky/blind/service/UserService.java
+++ blinddates/src/main/java/com/sky/blind/service/UserService.java
@@ -85,7 +85,7 @@ public class UserService {
         PageHelper.startPage(page, size);
         List<User> users = userMapper.selectAll();
         for (int i = 0; i < users.size(); i++) {
-            User user = users.get(0);
+            User user = users.get(i);
             if (user.getUserFace().equals("") || user.getEducation().equals("") || user.getEducation() == null) {
                 users.remove(i);
             }
